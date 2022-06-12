a797c3c985ec9c48c42c9a04feb8217396fed920
comstudynews <comstudynews@gmail.com>
2022-06-12 15:07:17 +0900

    chapter04

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+// function Food({favorite, something, papapapa}) {
+//   return (<h1>I like {favorite}</h1>);
+// }
+
+function Food(props) {
+  const {name, picture, rating} = props;
+  return (<>
+    <h2>{name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} title={name} />
+  </>);
+}
 
 function App() {
+  const foodLikes = [
+    {id: 1, rating:5,  name : "Kimchi", image : "https://www.bing.com/th?id=AMMS_08ae5fefd45ebce4375319fbdc5561ef&w=214&h=282&rs=2&qlt=80&o=6&cdv=1&pid=16.1"},
+    {id: 2, rating:3.5, name : "Kimbap", image : "https://www.bing.com/th?id=AMMS_8a4d4ac74222d01c189100cbe094f163&w=214&h=214&rs=2&qlt=80&o=6&cdv=1&pid=16.1"},
+    {id: 3, rating:4, name : "Tomato", image : "https://www.bing.com/th?id=AMMS_9fbef684c4c8889f2975823f08db4318&w=214&h=322&rs=2&qlt=80&o=6&cdv=1&pid=16.1"}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodLikes.map(function (dish, i) {
+        return (<Food key={dish.id} rating={dish.rating} name={dish.name} picture={dish.image} />);
+      })}
     </div>
   );
+}
+
+//https://github.com/facebook/prop-types
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
 }
 
 export default App;
